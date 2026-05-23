503b5d7403a30ebdf6bb290580eaadb056291db8
RedRotto <jrojo_22@alu.uabcs.mx>
2026-05-23 01:31:20 -0700

    Adjusting Node connections to work with URI

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,23 @@
 const { MongoClient } = require('mongodb');
 
-const url = process.env.MONGO_URI || 'mongodb://localhost:27017';
-const client = new MongoClient(url);
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri, {
+  retryWrites:  true,
+  retryReads:   true,
+  serverSelectionTimeoutMS: 5000,
+  connectTimeoutMS: 10000
+});
 
 let db;
 
 const conectar = async () => {
   await client.connect();
   db = client.db('nexus_banca');
-  console.log('Conectado a MongoDB');
+  console.log('✔ Conectado');
 };
 
 const getDb = () => {
-  if (!db) throw new Error('Base de datos no inicializada');
+  if (!db) throw new Error('✘ Base de datos no inicializada');
   return db;
 };
 
